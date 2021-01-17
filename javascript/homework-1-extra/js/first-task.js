098451ad6d6e65c81168b3df2ae633577ee7904a
Bevzyk Soi <bevzyk.soi@gmail.com>
2021-01-17 18:52:11 +0200

    Homework 1-extra completed

---
+++ javascript/homework-1-extra/js/first-task.js
@@ -0,0 +1,25 @@
+//Перше завдання
+
+let str = "Привіт";
+let num = 123;
+let flag = true;
+const txt = "true";
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+//Друге завдання
+
+let e = 1;
+let f = "1";
+let g = true;
+let h = null;
+let i = NaN;
+
+const Kostia = true;
+const Rinat = null;
+const Emil = NaN;
+const Nastia = "Nastia";
+const Marko = 1000;
