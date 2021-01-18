b88961855520908299f31b21a5330b5b2340cf3b
Bevzyk Soi <bevzyk.soi@gmail.com>
2021-01-18 19:37:27 +0200

    Homework 3-extra completed

---
+++ javascript/homework-3-extra/js/script.js
@@ -0,0 +1,50 @@
+//Перше завдання
+
+// let k1 = Number.parseInt("5px");
+// let k2 = Number.parseInt("12djd334");
+// let k3 = Number.parseInt("12.45asdwe12");
+// let k4 = Number.parseInt("qwqweeewq");
+
+
+//Друге завдання
+
+// let k1 = Number.parseFloat("5px");
+// let k2 = Number.parseFloat("12djd334");
+// let k3 = Number.parseFloat("12.45asdwe12");
+// let k4 = Number.parseFloat("qwqweeewq");
+
+//Третє завдання
+
+// console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+// console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+//Четверте завдання
+
+// console.log(Number.parseInt(Math.random() * (19 - 3) + 1));
+
+//П'яте завдання
+
+// let result = 5 + 5 + '5';
+// console.log(result);
+// console.log(typeof result);
+
+//Шосте завдання
+
+// const email = "bevzyk.soi@gmail.com";
+// const hasEmailDog = email.includes("@");
+// const lengthOfEmail = email.length;
+// console.log(hasEmailDog + " " + lengthOfEmail);
+
+//Сьоме завдання
+
+// const stringMy = "My ";
+// const stringName = "name ";
+// const stringIs = "is ";
+// const fullName = stringMy + stringName + stringIs + "Viktor!";
+// console.log(fullName);
+
+//Восьме завдання
+
+const userName = "Олександро";
+const payment = 300;
+alert("Дякуємо, " + userName + "! " + "До сплати " + payment + " гривень.");
