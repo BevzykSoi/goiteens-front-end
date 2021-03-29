de0802de4ab19f580f6d29ea2ce7ae3150b0e63c
Bevzyk Soi <bevzyk.soi@gmail.com>
2021-03-29 18:12:58 +0300

    Homework 3 completed

--- javascript/homework-3/js/task-3.js
+++ javascript/homework-3/js/task-3.js
@@ -12,28 +12,25 @@ const findBestEmployee = function (employees) {
   }
 };
 
-console.log(
-  findBestEmployee({
-    ann: 29,
-    david: 35,
-    helen: 1,
-    lorence: 99,
-  }),
-); // lorence
+findBestEmployee({
+  ann: 29,
+  david: 35,
+  helen: 1,
+  lorence: 99,
+})
+// lorence
 
-console.log(
-  findBestEmployee({
-    poly: 12,
-    mango: 17,
-    ajax: 4,
-  }),
-); // mango
+findBestEmployee({
+  poly: 12,
+  mango: 17,
+  ajax: 4,
+})
+// mango
 
-console.log(
-  findBestEmployee({
-    lux: 147,
-    david: 21,
-    kiwi: 19,
-    chelsy: 38,
-  }),
-); // lux
+findBestEmployee({
+  lux: 147,
+  david: 21,
+  kiwi: 19,
+  chelsy: 38,
+})
+// lux
