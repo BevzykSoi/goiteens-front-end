0aef959600f4600152250b490b65e9c018e681d6
Bevzyk Soi <bevzyk.soi@gmail.com>
2021-03-29 18:10:06 +0300

    Homework 3 completed

---
+++ javascript/homework-3/js/task-2.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const countProps = function (obj) {
+    console.log(Object.entries(obj).length);
+};
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }));
